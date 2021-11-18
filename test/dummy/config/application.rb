86ca6d6aed4fd413cf213b6ed0cbd604ec1cec81
Jack Spiva <johnspiva@gmail.com>
2021-11-18 11:49:30 -0600

    Update application.rb

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -14,7 +14,6 @@ module Dummy
 
     config.active_job.queue_adapter = :test
     config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-    config.load_defaults "6.0" # enables zeitwerk mode in CRuby
-
+    config.autoloader = :zeitwerk
   end
 end
